2b45a11d2ce03b01843e38afd5d424fb4c3a585a
aecahill <aecsk8gr8@yahoo.com>
2015-11-10 18:03:12 +0100

    better_comments_8seqs
    Thoroughly commented version

--- extract8seqs.R
+++ extract8seqs.R
@@ -1,7 +1,7 @@
 #This is a program that takes a list of read names and well names and creates a fasta file of the reads we want for a single locus.
-#use for DIPLOID LOCI!!!
-#input: text file with list of reads and well names associated (2 reads per well!!!)
-#for now, header on text file needs to read "read1", "read2", "ratio1", "well"
+#use for PARALOGOUS LOCI!!!
+#input: text file with list of reads and well names associated (8 reads per well!!!)
+#for now, header on text file needs to read "read1", "read2"..."read8", "well"
 #also need working directory to contain all fasta files for a locus (.unique are best b/c smallest)
 #NB: will break if the names of the wells are not represented in the file list
 #goodreads table is based on the count_table code
