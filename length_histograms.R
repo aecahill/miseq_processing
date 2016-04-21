5204c7828da9e9df25135c2c5fe5e78a18787435
aecahill <aecsk8gr8@yahoo.com>
2016-04-21 11:13:57 +0200

    commented_histograms
    histogram code with better comments

--- length_histograms.R
+++ length_histograms.R
@@ -1,10 +1,10 @@
 lengthfiles<-list.files() #create list of all files in the working directory
 
-x0<-1
-breakpoints<-seq(x0, 250, 1)
+x0<-1 #set start of breakpoint sequence
+breakpoints<-seq(x0, 250, 1) #create 1 bp bins for histogram
 
 for (i in lengthfiles) {
   
   t<-read.table(i,header=FALSE) #read file
-  hist(t$V4,main=i,breaks=breakpoints)
+  hist(t$V4,main=i,breaks=breakpoints) #create histogram
 }
